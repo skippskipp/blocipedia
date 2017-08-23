715f52944cbf7c2b592a89d85981adefc8c07614
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-08-22 22:55:39 -0400

    Adds Faker gem seed data

--- blocipedia/db/seeds.rb
+++ blocipedia/db/seeds.rb
@@ -1,1 +1,24 @@
-require 'random_data'
+require 'faker'
+
+# Create Users
+5.times do
+     User.create!(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        username: Faker::Name.first_name
+      )
+end
+users = User.all
+
+# Create Chuck Norris Wikis
+10.times do
+  Wiki.create!(
+  title: Faker::ChuckNorris.fact,
+  body: Faker::Lorem.sentence(3),
+  user: users.sample
+  )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
